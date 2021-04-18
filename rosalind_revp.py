009135825306dcb269877ffb41c8c11c337f0831
FortressBio <60721863+FortressBio@users.noreply.github.com>
2021-04-17 23:36:33 -0400

    Update rosalind_revp.py

--- rosalind_revp.py
+++ rosalind_revp.py
@@ -5,7 +5,7 @@
 #
 # Solution to Rosalind problem: Locating Restriction Sites
 #
-# Given: Given: A DNA string of length at most 1 kbp in FASTA format.
+# Given: A DNA string of length at most 1 kbp in FASTA format.
 # Return: The position and length of every reverse palindrome in the string having 
 #         length between 4 and 12. You may return these pairs in any order.
 #
